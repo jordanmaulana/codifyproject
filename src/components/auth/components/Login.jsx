d14fdf7680ee688d38775423a61f16daaeaff97d
adgojoe <jmaulana@adgorithmics.com>
2023-11-27 21:48:18 +0700

    add more responsiveness

--- src/components/auth/components/Login.jsx
+++ src/components/auth/components/Login.jsx
@@ -4,6 +4,7 @@ import { Button, Input } from "@nextui-org/react";
 import Link from "next/link";
 import { useLogin } from "../hooks/useLogin";
 import { Eye, EyeOff } from "lucide-react";
+import Image from "next/image";
 
 export const Login = () => {
   const {
@@ -16,6 +17,16 @@ export const Login = () => {
 
   return (
     <main className="space-y-3 p-24 rounded-lg flex flex-col shadow-xl">
+      <div className="block lg:hidden">
+        <div className="relative w-full h-[64px] ">
+          <Image
+            src="/logo.png"
+            fill
+            className="object-scale-down bg-gradient-to-br from-pink to-purple rounded-xl"
+            alt="logo"
+          />
+        </div>
+      </div>
       <h1 className="text-lg font-semibold">Log in</h1>
       <Input name="email" type="email" label="Email Address" />
       <Input
@@ -40,7 +51,7 @@ export const Login = () => {
       >
         Login
       </Button>
-      <div className="flex gap-1">
+      <div className="flex gap-1 text-sm">
         <div>Don't have an account ?</div>
         <Link href="/register" className="text-blue-600 underline">
           Register here

38223340910da544ae7c499e8b941b20d0a59577
adgojoe <jmaulana@adgorithmics.com>
2023-11-27 19:12:06 +0700

    register & login background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        people: "url('/banner_bg.png')",
+      },
+      colors: {
+        pink: "#ED4690",
+        purple: "#5522CC",
+      },
+    },
   },
   plugins: [],
 };

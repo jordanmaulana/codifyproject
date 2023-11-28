5e386e48613fba3c2cb925f9f4116fd3b28e785d
adgojoe <jmaulana@adgorithmics.com>
2023-11-28 16:34:51 +0700

    fix bug middleware home redirection

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,11 @@ import { NextResponse } from "next/server";
 export default function middleware(request) {
   // Verifikasi TOKEN
   // Ada atau Tidak
+
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.rewrite(new URL("/login", request.url));
+  }
+
   const cookie = request.cookies.get("token")?.value;
 
   if (cookie) {

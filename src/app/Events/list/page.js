b15ce1feabb59f62fda98d1456c26bbbefad0777
adgojoe <jmaulana@adgorithmics.com>
2023-11-30 23:53:20 +0700

    remove events/list folder

--- src/app/Events/list/page.js
+++
@@ -1,8 +0,0 @@
-import ListEvent from "@/components/events/components/ListEvent";
-import React from 'react'
-
-export default function Page() {
-  return (
-    <ListEvent />
-  )
-}
--- src/app/Events/page.js
+++ src/app/Events/page.js
@@ -1,41 +1,6 @@
-import { Card } from "@/components/Card";
+import ListEvent from "@/components/events/components/ListEvent";
+import React from "react";
 
-async function getEvents(){
-  const res = await fetch("https://eventmakers-api.vercel.app/api/event",{
-    cache: "no-store"
-  });
-  const result = await res.json();
-  //console.log(result);
-  return result;
-}
-
-export default async function Events() {
-  
-  const { data } = await getEvents();
-  
-  return (
-    <div className="flex flex-row justify-center h-screen bg-slate-100">
-      <div className="max-h-screen overflow-y-auto">
-          <div className="grid grid-cols-1 lg:grid-cols-2 m-1">
-              {data?.map((data) => {
-                return (
-                  <Card 
-                  id={data.id}
-                  createdAt={data.createdAt}
-                  updatedAt={data.updatedAt}
-                  name={data.name}
-                  description={data.description}
-                  location={data.location}
-                  date={data.date}
-                  isBanned={data.isBanned}
-                  authorId={data.authorId}
-                  participants={data.participants}
-                  author={data.author}
-                    />
-                );
-              })}
-          </div>
-      </div>
-    </div>
-  )
+export default function Page() {
+  return <ListEvent />;
 }

94b7a8c183bf4818551a011dbf7c0a9c432e499d
rifqiizz <rifqi.izzoboy@gmail.com>
2023-11-30 22:50:20 +0700

    Event list functionality and basic ui - done

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -4,3 +4,4 @@ export const REGISTER_URL = `${API_URL}/auth/register`;
 export const LOGIN_URL = `${API_URL}/auth/login`;
 
 export const CREATE_EVENT_URL = `${API_URL}/events`;
+export const GET_EVENT_URL = `${API_URL}/events`;

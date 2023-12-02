e5c2e1d30dfc5917e62f147983d679300c6057bc
adgojoe <jmaulana@adgorithmics.com>
2023-12-02 15:13:49 +0700

    clicking codivent header redirect to all event page

--- src/components/shared-ui/Header.jsx
+++ src/components/shared-ui/Header.jsx
@@ -5,12 +5,14 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header className="flex justify-between py-4 px-8 items-center text-white ">
-      <div className="text-lg">
-        <span className="font-extrabold text-lg">Codi</span>
-        <span className="text-lg">vent</span>
-      </div>
+      <Link href="/events">
+        <div className="text-lg">
+          <span className="font-extrabold text-lg">Codi</span>
+          <span className="text-lg">vent</span>
+        </div>
+      </Link>
       <div className="flex gap-8 items-center">
-        <Link href="/events#">Explore</Link>
+        <Link href="/events">Explore</Link>
         <Link href="/events/myevents">My Events</Link>
         <Link href="#footer">Contact Us</Link>
         <CustomAvatar />
